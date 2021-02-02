e3afebea8a93b1901e57ef2f928c195ffda61893
Mandy <mandypletinckx13@gmail.com>
2021-02-02 08:55:06 +0100

    'animation/firebase'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import './App.css'
 import Formulaire from './components/Formulaire'
 import Message from './components/Message'
 
-class App extends Component {
+// Firebase
+import base from './base'
 
-  render() {
-    return (
-        <div className="box">
-          <div>
-            <div className="messages">
-              <Message />
-              <Message />
-              <Message />
+// Animations
+import {CSSTransition, TransitionGroup} from 'react-transition-group'
+import './animations.css'
+
+class App extends Component {
+    state = {
+        messages: {},
+        pseudo: this.props.match.params.pseudo
+    }
+
+    messagesRef = createRef()
+
+    // au moment ou l'application se monte
+    componentDidMount () {
+        base.syncState('/', {
+            context: this,
+            state: 'messages'
+        })
+    }
+    // pour faire remonter ma page à chaque fois que j'ai un nouveau message
+    componentDidUpdate () {
+        // faire référence à un élément
+        const ref = this.messagesRef.current // fait référence à l'élément sur lequel la ref est appelée
+        ref.scrollTop = ref.scrollHeight
+    }
+
+    addMessage = message => {
+        const messages = {...this.state.messages}
+        messages[`message-${Date.now()}`] = message
+        Object.keys(messages).slice(0,-10).forEach(key => {
+            messages[key] = null
+        })
+        this.setState({messages})
+    }
+
+    isUser = pseudo => pseudo === this.state.pseudo
+
+    render() {
+        const messages = Object.keys(this.state.messages).map(
+            key => (
+                <CSSTransition
+                    key={key}
+                    timeout={200}
+                    classNames='fade'
+                >
+                    <Message
+                        isUser={this.isUser}
+                        pseudo={this.state.messages[key].pseudo}
+                        message={this.state.messages[key].message}
+                    />
+                </CSSTransition>
+            )
+        )
+
+
+        return (
+            <div className="box">
+                <div>
+                    <div className="messages" ref={this.messagesRef}>
+                        <TransitionGroup className="message">
+                            {messages}
+                        </TransitionGroup>
+                    </div>
+                </div>
+                <Formulaire
+                    length={140}
+                    pseudo={this.state.pseudo}
+                    addMessage={this.addMessage}
+                />
             </div>
-          </div>
-          <Formulaire />
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react';
 
 class Formulaire extends Component {
+    state = {
+        message:'',
+        length: this.props.length
+    }
+
+    createMessage = () => {
+        const {addMessage, pseudo, length} = this.props
+
+        const message = {
+            pseudo: pseudo,
+            message: this.state.message
+        }
+
+        addMessage(message)
+
+        // Reset
+        this.setState({message:'', length:length})
+
+    }
+
+    handleChange = event => {
+        const message = event.target.value
+        const length = this.props.length - message.length
+        this.setState({message:message, length:length})
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.createMessage()
+    }
+
+    handleKeyUp = event => {
+        if(event.key === 'Enter')
+        {
+            this.createMessage()
+        }
+    }
+
     render() {
         return (
-            <form className="form">
+            <form className="form" onSubmit={this.handleSubmit}>
                 <textarea
+                    value={this.state.message}
+                    onChange={this.handleChange}
+                    onKeyUp={this.handleKeyUp}
                     required
-                    maxLength="140"
+                    maxLength={this.props.length}
                 />
                 <div className="info">
-                    140
+                    {this.state.length}
                 </div>
                 <button type="submit">
                     Envoyer
